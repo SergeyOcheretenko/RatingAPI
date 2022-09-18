dbb517d58a4d0710788cae4a346346f2dc16a5dc
Sergey Ocheretenko <ocheretenko.s@gmail.com>
2022-09-18 20:28:48 +0300

    Create modules structure
    Squashed commit of the following:
    
    commit dfb63fc9d5f1b140d5e82f4108542d65be40f3d4
    Author: Sergey Ocheretenko <ocheretenko.s@gmail.com>
    Date:   Sun Sep 18 20:27:24 2022 +0300
    
        feat: add modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { RatingPageModule } from './rating-page/rating-page.module';
+import { ProductModule } from './product/product.module';
+import { ReviewModule } from './review/review.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, RatingPageModule, ProductModule, ReviewModule],
   controllers: [AppController],
   providers: [AppService],
 })
