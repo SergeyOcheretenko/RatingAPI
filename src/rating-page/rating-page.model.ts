c6e367286ddf73c2a620cacad69eefb2b0653259
Sergey Ocheretenko <ocheretenko.s@gmail.com>
2022-11-20 10:01:21 +0200

    feat: finish rating page model

--- src/rating-page/rating-page.model.ts
+++ src/rating-page/rating-page.model.ts
@@ -8,6 +8,28 @@ export enum TopLevelCategory {
   PRODUCTS = 'products',
 }
 
+export class UpworkData {
+  @prop()
+  count: number;
+
+  @prop()
+  juniorSalary: number;
+
+  @prop()
+  middleSalary: number;
+
+  @prop()
+  seniorSalary: number;
+}
+
+export class RatingPageAdvantage {
+  @prop()
+  title: string;
+
+  @prop()
+  description: string;
+}
+
 export interface RatingPageModel extends Base, TimeStamps {}
 export class RatingPageModel {
   @prop({ enum: TopLevelCategory })
@@ -16,22 +38,27 @@ export class RatingPageModel {
   @prop()
   secondCategory: string;
 
+  @prop({ unique: true })
+  alias: string;
+
   @prop()
   title: string;
 
   @prop()
   category: string;
-  GRC?: {
-    count: number;
-    juniorSalary: number;
-    middleSalary: number;
-    seniorSalary: number;
-  };
-  advantages: {
-    title: string;
-    description: string;
-  }[];
+
+  @prop({ type: () => UpworkData })
+  upwork?: UpworkData;
+
+  @prop({ type: () => [RatingPageAdvantage] })
+  advantages: RatingPageAdvantage[];
+
+  @prop()
   seoText: string;
+
+  @prop()
   tagsTitle: string;
+
+  @prop({ type: () => [String] })
   tags: string[];
 }
